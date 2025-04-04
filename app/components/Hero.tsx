2c2b90b0b9346318a81a6ddbacb39cff0e00d467
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-04-05 08:58:30 +0900

    updated hero, schedule

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,11 +13,11 @@ export default function Hero() {
           className="object-cover -z-50 brightness-80"
         />
       </div>
-      <div className="flex flex-col justify-center items-center mt-140 md:mt-140 lg:mt-90 2xl:mt-140 max-w-7xl mx-auto">
-        <h1 className="font-bold text-8xl md:text-9xl 2xl:text-[150px] tracking-tight">
+      <div className="flex flex-col justify-center items-center mt-70 md:mt-100 lg:mt-58 2xl:mt-80 max-w-7xl mx-auto">
+        <h1 className="font-bold text-8xl md:text-9xl 2xl:text-[170px] tracking-tight">
           DIONELA
         </h1>
-        <p className="w-[520px] text-center pt-4 px-16 md:px-0 text-gray-300">
+        <p className="w-[520px] text-center pt-4 px-16 md:px-0 text-gray-300 tracking-tighter">
           Join Dionela for an unforgettable live performance! Get your tickets
           now for a night of great music and unforgettable moments.
         </p>
@@ -27,7 +27,7 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="2xl:w-1/3 mx-auto px-4">
+      <div className="w-full 2xl:w-2/3 mx-auto px-4 md:px-8 absolute bottom-5">
         <Schedule />
       </div>
     </div>
--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -1,8 +1,10 @@
 export default function Schedule() {
   return (
-    <div className="flex items-center justify-between lg:py-4 md:py-12 bg-gray-400/10 backdrop-blur-xs mt-48 lg:mt-24 rounded-4xl shadow-xs shadow-gray-900 text-gray-300 px-16">
+    <div className="flex items-center justify-between py-4 lg:py-4 md:py-8 bg-gray-400/10 backdrop-blur-xs mt-48 lg:mt-24 rounded-4xl shadow-xs shadow-gray-900 text-gray-300 px-16">
       <div className="flex items-center justify-evenly w-screen">
-        <h1 className="lg:text-lg font-semibold text-nowrap">June 13, 2025</h1>
+        <h1 className="font-semibold text-nowrap text-gray-300">
+          June 13, 2025
+        </h1>
         <p className="">|</p>
         <div className="text-center text-nowrap">
           <p className="pt-2 text-gray-400">Melrose Ballroom</p>
