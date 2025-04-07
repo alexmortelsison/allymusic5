8d2d1a6bf45383a12e7a8dbb262bfe373f6b2d08
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-04-07 20:11:30 +0900

    updated photoops page

--- app/photoops/page.tsx
+++ app/photoops/page.tsx
@@ -3,10 +3,12 @@ import DownloadImage from "../components/DownloadImage";
 export default function PhotoOpsPage() {
   return (
     <div className="max-w-7xl mx-auto">
-      <h1 className="px-14 lg:px-0 md:text-4xl text-center">
+      <h1 className="px-14 lg:px-0 md:text-4xl text-2xl text-center">
         Relive The Experience.
       </h1>
-      <p className="text-center pt-2">Download your image.</p>
+      <p className="text-center pt-2 text-sm md:text-md">
+        Download your image.
+      </p>
       <div className="pt-8 max-w-7xl mx-auto flex justify-center">
         <DownloadImage />
       </div>
